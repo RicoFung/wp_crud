5a0e8d9696607ae2f2be157a51dad2abd41905d7
JoshuaFun <evanole@163.com>
2018-03-03 22:30:09 +0800

    实现删除功能

--- src/main/java/com/admin/api/TbDemoApi.java
+++ src/main/java/com/admin/api/TbDemoApi.java
@@ -43,7 +43,7 @@ public class TbDemoApi extends BaseController<TbDemo>
 	{
 		try
 		{
-			service.del(CollectionUtil.toLongArray(req.getLongArray("id[]", 0l)));
+			service.del(CollectionUtil.strToLongArray(req.getString("tcRowid"), ","));
 			result.setSuccess(true);
 		}
 		catch(Exception e)
